044d624356e3f845c4e4c279a264f59e1e24861e
Eyad Aqrabawi <41790482+eyadaq@users.noreply.github.com>
2025-08-03 21:40:19 +0000

    Done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+	if (typeof(num1) !== 'number' || typeof(num2) !== 'number' 
+		|| !Number.isInteger(num1) || !Number.isInteger(num2))
+		return("ERROR");
+	if (num1 < 0 || num2 < 0)
+		return("ERROR");
+	let sum = 0;
+	let number = 0;
+	if (num2 > num1)
+	{
+		number = num1;
+		while (number <= num2)
+		{
+			sum += number;
+			number++;
+		}
+		return (sum);
+	}
+	number = num2;
+	while (number <= num1)
+	{
+		sum += number;
+		number++;
+	}
+	return (sum);
 };
 
 // Do not edit below this line
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+	if (year > 400)
+	{
+		if (year % 100 === 0)
+		{
+			if (year % 400 === 0)
+				return true;
+			return false;
+		}
+		if (year % 4 === 0)
+			return true;
+			
+	}
+	if (year % 4 === 0)
+		return true;
+	return false;
 };
 
 // Do not edit below this line
