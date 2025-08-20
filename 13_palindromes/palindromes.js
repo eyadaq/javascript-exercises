80450679ec6647f3a0641d3893d7bbc196886a01
eyadaq <eyadaq14@gmail.com>
2025-08-20 23:21:34 +0300

    palindrome Done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
-
-// Do not edit below this line
-module.exports = palindromes;
+const palindromes = function (s) {
+	s = s.replace(/[^a-z0-9]/gi , "").toLowerCase();
+	let l = Math.floor(s.length / 2);
+	for (let i = 0; i < l; i++) {
+	  if (s[i] !== s[s.length - i - 1]) {
+		return false;
+	  }
+	}
+	return true;
+  };
+  
+  // Do not edit below this line
+  module.exports = palindromes;
+  
