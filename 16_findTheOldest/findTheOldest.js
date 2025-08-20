0827026da58feda1f85a4219efa6f43a8be1002a
eyadaq <eyadaq14@gmail.com>
2025-08-20 23:39:51 +0300

    done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	let oldest = people[0];
+	let currentYear = new Date().getFullYear();
+	for (let i = 1; i < people.length; i++) {
+		let person = people[i];
 
-};
+		let deathYear = person.yearOfDeath || currentYear;
+		let age = deathYear - person.yearOfBirth;
 
-// Do not edit below this line
-module.exports = findTheOldest;
+		let oldestDeathYear = oldest.yearOfDeath || currentYear;
+		let oldestAge = oldestDeathYear - oldest.yearOfBirth;
+
+		if (age > oldestAge) {
+			oldest = person;
+		}
+	}
+	return oldest;
+  };
+  
+  // Do not edit below this line
+  module.exports = findTheOldest;
+  
