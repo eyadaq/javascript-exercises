ac785dc0ac82b02f11322bb86546beae29dce822
eyadaq <eyadaq14@gmail.com>
2025-08-20 21:45:28 +0300

    Refactor calculator functions to accept parameters and update tests to remove skips

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function(num , power) {
+   
+    return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(arr) {
+	if (arr === 0)
+    return 1;
+  else if (arr === 1)
+      return 1;
+  else if (arr < 0)
+      return 1;
+  else {
+    let num = arr;
+    let result = 1;
+    while (num > 1)
+    {
+      result *= num;
+      num--;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
